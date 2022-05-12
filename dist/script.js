24ceda0e8c0c8f20e0799a83cc3de651ed568901
sofydon <petinova@bk.ru>
2022-05-11 22:43:13 -0400

    add modals

--- dist/script.js
+++ dist/script.js
@@ -14010,8 +14010,56 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _slider__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./slider */ "./src/js/slider.js");
+/* harmony import */ var _modules_modals__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/modals */ "./src/js/modules/modals.js");
 
-console.log(1);
+
+window.addEventListener('DOMContentLoaded', () => {
+  Object(_modules_modals__WEBPACK_IMPORTED_MODULE_1__["default"])();
+});
+
+/***/ }),
+
+/***/ "./src/js/modules/modals.js":
+/*!**********************************!*\
+  !*** ./src/js/modules/modals.js ***!
+  \**********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+const modals = () => {
+  function bindModal(trigger, modal, close) {
+    trigger.addEventListener('click', e => {
+      if (e.target) {
+        e.preventDefault();
+      }
+
+      modal.style.display = "block"; // document.body.style.overflow = "hidden";
+
+      document.body.classList.add('modal-open');
+    });
+    close.addEventListener('click', () => {
+      modal.style.display = "none"; // document.body.style.overflow = "";
+
+      document.body.classList.remove('modal-open');
+    });
+    modal.addEventListener('click', e => {
+      if (e.target === modal) {
+        modal.style.display = "none"; // document.body.style.overflow = "";
+
+        document.body.classList.remove('modal-open');
+      }
+    });
+  }
+
+  const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
+        modalEngineer = document.querySelector('.popup_engineer'),
+        modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
+  bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (modals);
 
 /***/ }),
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 import "./slider";
+import modals from './modules/modals';
 
-console.log(1);
+window.addEventListener('DOMContentLoaded', () => {
+    modals();
+
+});
